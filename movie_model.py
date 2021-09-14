b30541cc802fd98ce296c066ee704811e69217dd
ssar <ssar@example.com>
2021-09-14 14:17:16 +0900

    moviesmodel 클래스 만들기

---
+++ movie_model.py
@@ -0,0 +1,6 @@
+class MoiveModel():
+    def __init__(self, ranking, summary, small_cover_image, title):
+        self.ranking = ranking
+        self.summary = summary
+        self.small_cover_image = small_cover_image
+        self.title = title
